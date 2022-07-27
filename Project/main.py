19fb99693be2a0f318e616467038b9bb5e0cb511
shanijacobson <shanijac55@gmail.com>
2022-07-27 15:10:51 +0300

    add original_video mode

--- Project/main.py
+++ Project/main.py
@@ -3,16 +3,10 @@ from Models.Vocabulary import GlossVocabulary, WordVocabulary
 from torch.utils.data import DataLoader, random_split
 from Models.TransformerModel import Encoder, TransformerSLT
 import os
-import gzip
-import pickle
 
 DATA_PATH = os.path.join("Data", "Phoenix14")
 BATCH_SIZE = 10
 VALIDATION_SIZE = 519
-
-
-def train_batch(batch):
-    pass
 
 
 def initialize():
@@ -36,13 +30,13 @@ def train_model():
                                             word_vocab=word_vocab, gloss_vocab=gloss_vocab)
     train_dataset, valid_dataset = random_split(total_train_dataset,
                                                 [len(total_train_dataset) - VALIDATION_SIZE, VALIDATION_SIZE])
-    # test_dataset = SignGlossLanguage(root=DATA_PATH, train=False, download=False,
-    #                                  word_vocab=word_vocab, gloss_vocab=gloss_vocab)
+    test_dataset = SignGlossLanguage(root=DATA_PATH, train=False, download=False,
+                                     word_vocab=word_vocab, gloss_vocab=gloss_vocab)
 
     # Data Loaders:
     train_loader = DataLoader(train_dataset, BATCH_SIZE, shuffle=True)
     validation_loader = DataLoader(valid_dataset, BATCH_SIZE, shuffle=True)
-    # test_loader = DataLoader(test_dataset, BATCH_SIZE, shuffle=False)
+    test_loader = DataLoader(test_dataset, BATCH_SIZE, shuffle=False)
     return train_loader, gloss_vocab, word_vocab
 
 
@@ -50,16 +44,10 @@ def test(train_loader, gloss_vocab, word_vocab):
     model = Encoder(1024, 3, 4, gloss_dim=len(gloss_vocab))
     model = TransformerSLT(1024, 3, 3, len(gloss_vocab), len(word_vocab), 4, spatial_flag=False)
     for frame, gloss, words in train_loader:
-        frame = frame.permute(1, 0, 2, 3)  # TODO: why can't batch size be in the first coordinate? Can you change in the model? If no lets move this to the dataset model
+        # TODO: Check if batch size can be in the first coordinate. If no move this to the dataset model
+        frame = frame.permute(1, 0, 2, 3)
         gloss = gloss.T
         words = words.T
-        #  gloss = torch.tensor([[[1 if token == elm else 0 for elm in gloss_vocab.get_itos()] for token in sent] for sent in gloss])
-        # gloss = torch.tensor([[gloss_vocab.get_itos().index(token) for token in sent] for sent in gloss])
-        # words = torch.tensor([[word_vocab.get_itos().index(token) for token in sent] for sent in words])
-
-        # words = torch.tensor([[[1 if token == elm else 0 for elm in word_vocab.get_itos()] for token in sent] for sent in words])
-        # gloss = gloss.permute(1,0,2)
-        # words = words.permute(1,0,2)
         model(frame, gloss, words)
 
 
